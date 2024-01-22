c6375bc2f6b2ff2c1d0d44a79afa11928f8864e2
vitviki <vtyagi1991@gmail.com>
2024-01-22 15:48:30 +0530

    hero section

---
+++ exercise_db/src/components/HeroBanner.jsx
@@ -0,0 +1,49 @@
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/banner.png";
+const HeroBanner = () => {
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight="700"
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat, smile <br /> and repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px" marginBottom={4}>
+        Checkout the most effective exercises
+      </Typography>
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#FF2625", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
+      <Typography
+        fontWeight={600}
+        color="#FF2625"
+        fontSize="200px"
+        sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+      >
+        Excercise
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+    </Box>
+  );
+};
+
+export default HeroBanner;
