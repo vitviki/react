3d01e47c4df8fe4b9f0ce1d1044b34e6fbfb4689
vitviki <vtyagi1991@gmail.com>
2024-01-23 18:28:26 +0530

    bodypart exercise display

--- exercise_db/src/components/ExerciseCard.jsx
+++ exercise_db/src/components/ExerciseCard.jsx
@@ -4,7 +4,7 @@ import { Button, Stack, Typography } from "@mui/material";
 const ExerciseCard = ({ exercise }) => {
   return (
     <Link to={`/exercise/${exercise.id}`} className="exercise-card">
-      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <img src={exercise.gifUrl} alt={exercise.name} loading="eager" />
       <Stack direction="row">
         <Button
           sx={{
--- exercise_db/src/components/Exercises.jsx
+++ exercise_db/src/components/Exercises.jsx
@@ -7,7 +7,7 @@ import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ setExercises, bodyPart, exercises }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const exercisesPerPage = 9;
+  const exercisesPerPage = 6;
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirtExercise = indexOfLastExercise - exercisesPerPage;
@@ -16,18 +16,38 @@ const Exercises = ({ setExercises, bodyPart, exercises }) => {
     indexOfLastExercise
   );
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exerciseData = [];
+      if (bodyPart === "all") {
+        exerciseData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises?limit=25",
+          exerciseOptions
+        );
+      } else {
+        exerciseData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+      setExercises([]);
+      setExercises(exerciseData);
+    };
+    fetchExercisesData();
+  }, [bodyPart]);
+
   const paginate = (e, value) => {
     setCurrentPage(value);
     window.scrollTo({ top: 1800, behavior: "smooth" });
   };
   return (
-    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
         Showing results
       </Typography>
       <Stack
         direction="row"
-        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        sx={{ gap: { lg: "107px", xs: "50px" } }}
         flexWrap="wrap"
         justifyContent="center"
       >
