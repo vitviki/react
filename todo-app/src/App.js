b8e828dc6624a741051327d3fb72740a2a006864
vitviki <vtyagi1991@gmail.com>
2024-04-02 10:49:14 +0530

    initial setup

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoWrappwer from "./Components/TodoWrapper";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrappwer />
+    </div>
+  );
+}
+
+export default App;
