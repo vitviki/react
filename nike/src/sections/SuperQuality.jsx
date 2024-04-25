bc05345bc1247acdac106f60d66f1e7d8c1ef468
vitviki <vtyagi1991@gmail.com>
2024-04-25 14:42:01 +0530

    Quality section

--- nike/src/sections/SuperQuality.jsx
+++ nike/src/sections/SuperQuality.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import Button from "../components/Button";
+import { shoe8 } from "../assets/images";
 
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex max-container w-full justify-between items-center max-lg:flex-col gap-10"
+    >
+      <div className="flex flex-col flex-1">
+        <h2 className="font-palanquin text-4xl font-bold lg:max-w-lg">
+          We provide you <span className="text-coral-red">Super Quality</span>{" "}
+          shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11 max-lg:mx-auto">
+          <Button label={"View details"} />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="Shoe 8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
