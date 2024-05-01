89d48fd00e6804ea411f4feae91285b4a8a0df4d
vitviki <vtyagi1991@gmail.com>
2024-05-01 13:22:20 +0530

    OpenShop: recent orders and popular products

--- open-shop/src/sections/PopularProducts.jsx
+++ open-shop/src/sections/PopularProducts.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { POPULAR_PRODUCTS } from "../constants/index";
 
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <div className="bg-white w-[20rem] h-[22rem] shadow-lg p-4 border border-gray-200 overflow-auto">
+      <strong className="text-gray-700 font-montserrat">
+        Popular Products
+      </strong>
+      <div className=" mt-4 flex flex-col gap-3">
+        {POPULAR_PRODUCTS.map((product) => (
+          <Link className="flex" key={product.id}>
+            <div className="w-10 h-10 min-w-10 bg-gray-200">
+              <img
+                src={product.product_thumbnail}
+                alt={product.product_name}
+                className="w-full h-full object-over rounded-sm overflow-hidden"
+              />
+            </div>
+            <div className="ml-4  font-palanquin flex-1 hover:underline">
+              <p className="text-base text-gray-900">{product.product_name}</p>
+              <span
+                className={`text-sm font-medium ${
+                  product.product_stock === 0
+                    ? "text-red-500"
+                    : "text-green-500"
+                }`}
+              >
+                {product.product_stock !== 0
+                  ? product.product_stock
+                  : "Out of stock"}
+              </span>
+            </div>
+            <div className=" font-montserrat text-sm font-semibold">
+              {product.product_price}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PopularProducts;
--- open-shop/src/sections/RecentOrders.jsx
+++ open-shop/src/sections/RecentOrders.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { RECENT_ORDER_DATA } from "../constants/index";
+import { getOrderStatus } from "../constants/utils/index";
 
 const RecentOrders = () => {
-  return <div>RecentOrders</div>;
+  return (
+    <div className="bg-white px-4 pt-3 rounded-sm border border-gray-200 shadow-lg flex-1 h-[22rem] overflow-auto">
+      <strong className="text-gray-700 font-montserrat">Recent Orders</strong>
+      <div className="mt-3">
+        <table className="w-full h-full border border-gray-100">
+          <thead className="">
+            <tr>
+              <td>ID</td>
+              <td>Product ID</td>
+              <td>Customer Name</td>
+              <td>Order Date</td>
+              <td>Order Total</td>
+              <td>Shipping Adress</td>
+              <td>Order Status</td>
+            </tr>
+          </thead>
+          <tbody className="">
+            {RECENT_ORDER_DATA.map((order) => (
+              <tr key={order.id}>
+                <td>#{order.id}</td>
+                <td>
+                  <Link
+                    className="text-sky-700"
+                    to={`/products/${order.product_id}`}
+                  >
+                    {order.product_id}
+                  </Link>
+                </td>
+                <td>
+                  <Link
+                    className="text-sky-700"
+                    to={`/users/${order.customer_name}`}
+                  >
+                    {order.customer_name}
+                  </Link>
+                </td>
+                <td>{new Date(order.order_date).toLocaleDateString()}</td>
+                <td>{order.order_total}</td>
+                <td>{order.shipment_address}</td>
+                <td>{getOrderStatus(order.current_order_status)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default RecentOrders;
